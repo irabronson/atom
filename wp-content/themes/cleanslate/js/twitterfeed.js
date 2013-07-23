e0de3b09cd06be87936c6f1f821316919184bee0
Saki Sato <sakiisato@gmail.com>
2013-07-23 11:57:17 -0400

    tweaked twitter feed, showed retweets, added time

--- wp-content/themes/cleanslate/js/twitterfeed.js
+++ wp-content/themes/cleanslate/js/twitterfeed.js
@@ -1,87 +1,111 @@
 //JQuery Twitter Feed. Coded by Tom Elliott @ www.webdevdoor.com (2013) based on https://twitter.com/javascripts/blogger.js
 //Requires JSON output from authenticating script: http://www.webdevdoor.com/php/authenticating-twitter-feed-timeline-oauth/
-
-var twitterFeed = function(getTweetsURL, currentProfile, noOfTweets) {
+//Source:http://www.webdevdoor.com/javascript-ajax/custom-twitter-feed-integration-jquery/
+var twitterFeed = function(getTweetsURL, profile, container) {
     
     $j(document).ready(function () {
-        var displaylimit = noOfTweets;
-        var twitterprofile = currentProfile;
+        var displaylimit = 1;
+        var twitterprofile = profile;
+        var tweetParentContainer = $j(container);
+        var tweetContainer = $j(container).children('.tweet');
         var showdirecttweets = true;
-        var showretweets = false;
+        var showretweets = true;
         var showtweetlinks = false;
         var showprofilepic = false;
         var showretweetindicator = false;
+        
+        tweetParentContainer.show();
         
         var loadingHTML = '';
         
         loadingHTML += '<div id="loading-container">Loading...</div>';
         
-        $j('#twitter-feed').html(loadingHTML);
+        tweetContainer.html(loadingHTML);
         
-        var tweetsRequestUrl = getTweetsURL + '?currentUser=' + currentProfile;
+        var tweetsRequestUrl = getTweetsURL + '?currentUser=' + twitterprofile;
         
         $j.getJSON(tweetsRequestUrl, 
-            function(feeds) {   
-               // alert(feeds);
-                var feedHTML = '';
-                var displayCounter = 1;         
-                for (var i=0; i<feeds.length; i++) {
-                    var tweetscreenname = feeds[i].user.name;
-                    var tweetusername = feeds[i].user.screen_name;
-                    var profileimage = feeds[i].user.profile_image_url_https;
-                    var status = feeds[i].text; 
-                    var isaretweet = false;
-                    var isdirect = false;
-                    var tweetid = feeds[i].id_str;
-
-                    //If the tweet has been retweeted, get the profile pic of the tweeter
-                    if(typeof feeds[i].retweeted_status != 'undefined'){
-                       profileimage = feeds[i].retweeted_status.user.profile_image_url_https;
-                       tweetscreenname = feeds[i].retweeted_status.user.name;
-                       tweetusername = feeds[i].retweeted_status.user.screen_name;
-                       tweetid = feeds[i].retweeted_status.id_str;
-                       status = feeds[i].retweeted_status.text; 
-                       isaretweet = true;
-                     };
-
-
-                     //Check to see if the tweet is a direct message
-                     if (feeds[i].text.substr(0,1) == "@") {
-                         isdirect = true;
-                     }
-
-                    // console.log(feeds[i]);
-
-                     //Generate twitter feed HTML based on selected options
-                     if (((showretweets == true) || ((isaretweet == false) && (showretweets == false))) && ((showdirecttweets == true) || ((showdirecttweets == false) && (isdirect == false)))) { 
-                        if ((feeds[i].text.length > 1) && (displayCounter <= displaylimit)) {
-                            
-                            if (showtweetlinks == true) {
-                                status = addlinks(status);
-                            }
-                            
-                            feedHTML += '<div class="twitter-article">';                                                          
-                            
-                            feedHTML += '<div class="twitter-text"><p><span class="tweet-time"><a href="https://twitter.com/'+tweetusername+'/status/'+tweetid+'" target="_blank"></a></span><br/>'+status+'</p>';
-
-                            if ((isaretweet == true) && (showretweetindicator == true)) {
-                                feedHTML += '<div id="retweet-indicator"></div>';
-                            }
-                            
-                            feedHTML += '</div>';
-                            
-                            if (displayCounter === feeds.length) {
-                                feedHTML += '<div class="tweetprofilelink">Follow <a href="https://twitter.com/'+tweetusername+'" target="_blank">@'+tweetusername+'</a></div>';
-                            }
-                            
-                            feedHTML += '</div>';
-                            
-                            displayCounter++;
-                        }   
-                     }
-                }
+            function(feeds) {
                 
-                $j('#twitter-feed').html(feedHTML);
+                // console.log(feeds);
+                
+                var feedHTML = '';
+                
+                if ( feeds.errors ) {
+                    
+                    feedHTML = '<p>';
+                    feedHTML += 'Error: ';
+                    feedHTML += feeds.errors[0]['message'] + ' (' + feeds.errors[0]['code'] + ')';
+                    feedHTML += '</p>';
+                    
+                } else {
+                    
+                    var displayCounter = 1;
+                    
+                    for (var i=0; i<feeds.length; i++) {
+                        var tweetscreenname = feeds[i].user.name;
+                        var tweetusername = feeds[i].user.screen_name;
+                        var profileimage = feeds[i].user.profile_image_url_https;
+                        var status = feeds[i].text; 
+                        var isaretweet = false;
+                        var isdirect = false;
+                        var tweetid = feeds[i].id_str;
+                        
+                        //If the tweet has been retweeted, get the profile pic of the tweeter
+                        if(typeof feeds[i].retweeted_status != 'undefined'){
+                            profileimage = feeds[i].retweeted_status.user.profile_image_url_https;
+                            tweetscreenname = feeds[i].retweeted_status.user.name;
+                            tweetusername = feeds[i].retweeted_status.user.screen_name;
+                            tweetid = feeds[i].retweeted_status.id_str;
+                            status = feeds[i].retweeted_status.text; 
+                            isaretweet = true;
+                        };
+                        
+                        //Check to see if the tweet is a direct message
+                        if (feeds[i].text.substr(0,1) == "@") {
+                            isdirect = true;
+                        }
+                        
+                        // console.log(feeds[i]);
+                        
+                        //Generate twitter feed HTML based on selected options
+                        if (((showretweets == true) || ((isaretweet == false) && (showretweets == false))) && ((showdirecttweets == true) || ((showdirecttweets == false) && (isdirect == false)))) {
+                            
+                            if ((feeds[i].text.length > 1) && (displayCounter <= displaylimit)) {
+                                
+                                if (showtweetlinks == true) {
+                                    status = addlinks(status);
+                                }
+                                
+                                feedHTML += '<div class="twitter-article">';                                                          
+                                
+                                feedHTML += '<div class="twitter-text">';
+                                feedHTML += '<p>';
+                                feedHTML += '<span class="tweet-time">';
+                                feedHTML += '<a href="https://twitter.com/' + tweetusername + '/status/' + tweetid + '" target="_blank">' + relative_time(feeds[i].created_at) + '</a>';
+                                feedHTML += '</span>';
+                                feedHTML += '<br/>' + status + '</p>';
+                                
+                                if ((isaretweet == true) && (showretweetindicator == true)) {
+                                    feedHTML += '<div id="retweet-indicator"></div>';
+                                }
+                                
+                                feedHTML += '</div>';
+                                
+                                if (displayCounter === feeds.length) {
+                                    feedHTML += '<div class="tweetprofilelink">Follow <a href="https://twitter.com/'+tweetusername+'" target="_blank">@'+tweetusername+'</a></div>';
+                                }
+                                
+                                feedHTML += '</div>';
+                                
+                                displayCounter++;
+                            }
+                        }
+                    } //end for loop
+                    
+                } //end if else
+                
+                tweetContainer.html(feedHTML);
                 
         }).error(function(jqXHR, textStatus, errorThrown) {
             if (errorThrown == "Not Found") {
@@ -107,6 +131,33 @@ var twitterFeed = function(getTweetsURL, currentProfile, noOfTweets) {
                 return '<a href="https://twitter.com/search?q='+reply.substring(1)+'" style="font-weight:lighter;" target="_blank">'+reply.charAt(0)+reply.substring(1)+'</a>';
             });
             return data;
+        }
+        
+        function relative_time(time_value) {
+            var values = time_value.split(" ");
+            time_value = values[1] + " " + values[2] + ", " + values[5] + " " + values[3];
+            var parsed_date = Date.parse(time_value);
+            var relative_to = (arguments.length > 1) ? arguments[1] : new Date();
+            var delta = parseInt((relative_to.getTime() - parsed_date) / 1000);
+            var shortdate = time_value.substr(4,2) + " " + time_value.substr(0,3);
+            delta = delta + (relative_to.getTimezoneOffset() * 60);
+            
+            if (delta < 60) {
+                return '1m';
+            } else if(delta < 120) {
+                return '1m';
+            } else if(delta < (60*60)) {
+                return (parseInt(delta / 60)).toString() + 'm';
+            } else if(delta < (120*60)) {
+                return '1h';
+            } else if(delta < (24*60*60)) {
+                return (parseInt(delta / 3600)).toString() + 'h';
+            } else if(delta < (48*60*60)) {
+                //return '1 day';
+                return shortdate;
+            } else {
+                return shortdate;
+            }
         }
         
     });
